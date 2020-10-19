f0cf4378b8f956a947833792c29faf20172d0df3
Bruno Severo <brunoseverocamilo@gmail.com>
2020-10-19 16:17:57 -0300

    posts criados

--- public/feed/test.js
+++ public/feed/test.js
@@ -1,35 +1,14 @@
-function postarConteudo(){
-    let mensagem = document.getElementById('message').value
-    let email = localStorage.getItem("login");
-    if(mensagem != null){
-        postar(mensagem, email)
-    }
-    else
-        alert("É necessario escrever uma mensagem para postar!")
-  }
-  
-function postar(mensagem, email){
-    let http = new XMLHttpRequest()
-    let url = 'https://burnhop-backend.herokuapp.com/users/'
-    let dados = {
-        "texto": mensagem,
-        "email": email
-    }
-    
+var theUrl = "https://burnhop-backend.herokuapp.com/users/posts/get-all"
 
-    http.open('POST', url, true)
-    http.setRequestHeader('Content-type', 'application/json')
-    http.setRequestHeader('Access-Control-Allow-Origin', '*')
-    http.onload = function(){
-        console.log(http.responseText)
-        if(http.readyState == 4 && http.status == 200) {
-        alert("Conteúdo postado com sucesso");
-        window.location = ('/');
-        }
-        else{
-        alert("Erro ao postar conteudo")
-        }
-    }
 
-http.send(JSON.stringify(dados));
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    return xmlHttp.responseText;
 }
+
+
+var data = httpGet(theUrl);
+
+console.log(data.content);
